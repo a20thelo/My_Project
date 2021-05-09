472340ffb2e14bfa913b60c0d91c47e61fe6e2a3
there <a20thelo@student.his.se>
2021-05-09 11:48:22 +0200

    Added variabel and onclicklistener to button on AboutActivity that opens first activity (MainActivity).

--- app/src/main/java/com/example/myproject/AboutActivity.java
+++ app/src/main/java/com/example/myproject/AboutActivity.java
@@ -2,13 +2,31 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        button = (Button) findViewById(R.id.button_about);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
