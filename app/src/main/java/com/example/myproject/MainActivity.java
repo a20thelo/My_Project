a7ee52f451a60099c24300f76508dc81f4878371
there <a20thelo@student.his.se>
2021-05-10 16:03:08 +0200

    Added asset folder and a file with namne data.json, added my data to file.

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -2,10 +2,21 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -14,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a20thelo");
 
         button = (Button) findViewById(R.id.button_main);
         button.setOnClickListener(new View.OnClickListener() {
@@ -28,4 +41,51 @@ public class MainActivity extends AppCompatActivity {
         Intent intent  = new Intent (this, AboutActivity.class);
         startActivity(intent);
     }
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+        }
+    }
+
 }
