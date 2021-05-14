1e25d1716786d2c1023ca1bf66dc189a13b6affe
there <a20thelo@student.his.se>
2021-05-14 17:43:52 +0200

    Added local members of data.json to a new java class called Data.

---
+++ app/src/main/java/com/example/myproject/Data.java
@@ -0,0 +1,15 @@
+package com.example.myproject;
+
+public class Data {
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private String size;
+    private String cost;
+    private String auxdata;
+
+
+}
