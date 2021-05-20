18db794db6edac2a067d0e10c00b16976865d6a0
there <a20thelo@student.his.se>
2021-05-20 12:30:04 +0200

    Added toast to AboutActivity.

--- app/src/main/java/com/example/myproject/AboutActivity.java
+++ app/src/main/java/com/example/myproject/AboutActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
     private Button button;
@@ -14,6 +15,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        String about = new String("Appen är uttänkt för en målgrupp som befinner sig i skolåldern, allt mellan 8-12 år. Appens syfte ör att lära barn fakta om de största städerna, såsom geografisk plats, folkmängd med mera.");
+        Toast.makeText(getApplicationContext(),about, Toast.LENGTH_SHORT).show();
 
         button = (Button) findViewById(R.id.button_about);
         button.setOnClickListener(new View.OnClickListener() {
